71015388c779ffb9adae8b9fe80b7749fd7d8808
Cornel Izbasa <badc0ded@gmail.com>
2022-10-16 07:32:14 +0300

    Introduce expressions and arithmetic operations

--- include/tmpgfn/constant.h
+++ include/tmpgfn/constant.h
@@ -6,13 +6,71 @@
 namespace tmpgfn {
 namespace constants {
 
-template <typename V, V v,
-          typename std::enable_if_t<std::is_integral<V>::value, bool> = true>
-struct constant {
+template <typename V, V v> struct expression {
   using type = V;
   static constexpr V value = v;
-  using inc = constant<V, static_cast<V>(value + static_cast<V>(1))>;
-  using dec = constant<V, static_cast<V>(value - static_cast<V>(1))>;
+  static constexpr bool is_expression = true;
+};
+
+template <typename V, V v,
+          typename std::enable_if_t<std::is_integral<V>::value, bool> = true>
+struct constant : public expression<V, v> {
+  using type = V;
+  static constexpr V value = v;
+};
+template <typename V> using one = constant<V, static_cast<V>(1)>;
+template <typename V> using zero = constant<V, static_cast<V>(0)>;
+
+template <typename E1, typename E2,
+          typename std::enable_if_t<E1::is_expression, bool> = true,
+          typename std::enable_if_t<E2::is_expression, bool> = true,
+          typename std::enable_if_t<
+              std::is_same_v<typename E1::type, typename E2::type>, bool> =
+              true>
+struct add
+    : public expression<typename E1::type,
+                        static_cast<typename E1::type>(E1::value + E2::value)> {
+};
+
+template <typename E1,
+          typename std::enable_if_t<E1::is_expression, bool> = true>
+struct increment : add<E1, one<typename E1::type>> {};
+
+template <typename E1, typename E2,
+          typename std::enable_if_t<E1::is_expression, bool> = true,
+          typename std::enable_if_t<E2::is_expression, bool> = true,
+          typename std::enable_if_t<
+              std::is_same_v<typename E1::type, typename E2::type>, bool> =
+              true>
+struct subtract
+    : public expression<typename E1::type,
+                        static_cast<typename E1::type>(E1::value - E2::value)> {
+};
+
+template <typename E1,
+          typename std::enable_if_t<E1::is_expression, bool> = true>
+struct decrement : subtract<E1, one<typename E1::type>> {};
+
+template <typename E1, typename E2,
+          typename std::enable_if_t<E1::is_expression, bool> = true,
+          typename std::enable_if_t<E2::is_expression, bool> = true,
+          typename std::enable_if_t<
+              std::is_same_v<typename E1::type, typename E2::type>, bool> =
+              true>
+struct multiply
+    : public expression<typename E1::type,
+                        static_cast<typename E1::type>(E1::value *E2::value)> {
+};
+
+template <typename E1, typename E2,
+          typename std::enable_if_t<E1::is_expression, bool> = true,
+          typename std::enable_if_t<E2::is_expression, bool> = true,
+          typename std::enable_if_t<
+              std::is_same_v<typename E1::type, typename E2::type>, bool> =
+              true>
+struct divide
+    : public expression<typename E1::type,
+                        static_cast<typename E1::type>(E1::value / E2::value)> {
 };
 
 } // namespace constants
--- test_tmpgfn.cpp
+++ test_tmpgfn.cpp
@@ -6,26 +6,26 @@ static void check_lists() {
   using namespace tmpgfn::lists;
 
   // the first of the list of types: [float, char] is: float.
-  static_assert(std::is_same<types<float, char>::first_t, float>::value);
+  static_assert(std::is_same_v<types<float, char>::first_t, float>);
   // the rest of the list of types: [int, float, char] is: [float, char].
   static_assert(
-      std::is_same<types<int, float, char>::rest_t, types<float, char>>::value);
+      std::is_same_v<types<int, float, char>::rest_t, types<float, char>>);
   // the first of the rest of the list of types [int, float, char] is:
   // float.
   static_assert(
-      std::is_same<types<int, float, char>::rest_t::first_t, float>::value);
+      std::is_same_v<types<int, float, char>::rest_t::first_t, float>);
   using a_type_list = types<int, double, float>;
   // by inserting the first element of a list, in front of the
   // rest of the elements of the list, we get the list itself.
   static_assert(
-      std::is_same<a_type_list::rest_t::insert_front_t<a_type_list::first_t>,
-                   a_type_list>::value);
+      std::is_same_v<a_type_list::rest_t::insert_front_t<a_type_list::first_t>,
+                     a_type_list>);
   static_assert(types<int, double, float>::length == 3);
   // the length of the empty list is zero.
   static_assert(nil::length == 0);
 
   using extended_type_list = a_type_list::insert_front_t<char>;
-  static_assert(std::is_same<extended_type_list::first_t, char>::value);
+  static_assert(std::is_same_v<extended_type_list::first_t, char>);
   static_assert(extended_type_list::length == 4);
 
   using empty_list = nil::rest_t;
@@ -33,15 +33,15 @@ static void check_lists() {
   // inserting an element in front of the empty list and
   // inserting the same element at the back of the empty list
   // results in the same list.
-  static_assert(std::is_same<empty_list::insert_front_t<int>,
-                             empty_list::insert_back_t<int>>::value);
+  static_assert(std::is_same_v<empty_list::insert_front_t<int>,
+                               empty_list::insert_back_t<int>>);
 
-  static_assert(std::is_same<types<int, char>::append_t<types<double, float>>,
-                             types<int, char, double, float>>::value);
-  static_assert(std::is_same<nil::append_t<types<double, float>>,
-                             types<double, float>>::value);
+  static_assert(std::is_same_v<types<int, char>::append_t<types<double, float>>,
+                               types<int, char, double, float>>);
+  static_assert(std::is_same_v<nil::append_t<types<double, float>>,
+                               types<double, float>>);
   static_assert(
-      std::is_same<types<int, char>::append_t<nil>, types<int, char>>::value);
+      std::is_same_v<types<int, char>::append_t<nil>, types<int, char>>);
 }
 
 static void check_maps() {
@@ -51,41 +51,57 @@ static void check_maps() {
   using a_type_map =
       map<types<types<int, char>, types<double, float>, types<char, double>>>;
   // the value corresponding to key int is: char.
-  static_assert(std::is_same<a_type_map::get_t<int>, char>::value);
-  static_assert(std::is_same<a_type_map::get_t<double>, float>::value);
-  static_assert(std::is_same<a_type_map::get_t<char>, double>::value);
+  static_assert(std::is_same_v<a_type_map::get_t<int>, char>);
+  static_assert(std::is_same_v<a_type_map::get_t<double>, float>);
+  static_assert(std::is_same_v<a_type_map::get_t<char>, double>);
 
   // extend a map with a key-value pair, overriding
   // the previous value for key int (char) with double.
   using extended_type_map = a_type_map::set_t<int, double>;
-  static_assert(std::is_same<
-                extended_type_map,
-                map<types<types<int, double>, types<int, char>,
-                          types<double, float>, types<char, double>>>>::value);
-  static_assert(std::is_same<extended_type_map::get_t<int>, double>::value);
+  static_assert(
+      std::is_same_v<extended_type_map,
+                     map<types<types<int, double>, types<int, char>,
+                               types<double, float>, types<char, double>>>>);
+  static_assert(std::is_same_v<extended_type_map::get_t<int>, double>);
 
   using empty_map = map<nil>;
   // a map is a list of type pairs.
-  static_assert(
-      std::is_same<empty_map::set_t<int, char>,
-                   map<nil::insert_front_t<types<int, char>>>>::value);
+  static_assert(std::is_same_v<empty_map::set_t<int, char>,
+                               map<nil::insert_front_t<types<int, char>>>>);
 }
 
 void check_constants() {
   using namespace tmpgfn::constants;
 
-  using i_3 = constant<int, 3>;
-  static_assert(i_3::value == 3);
-  using i_4 = i_3::inc;
-  static_assert(i_4::value == 4);
-  using i_2 = i_4::dec::dec;
+  using i_1 = one<int>;
+  static_assert(std::is_same_v<i_1::type, int>);
+  static_assert(i_1::value == 1);
+  using i_2 = add<i_1, i_1>;
+  static_assert(std::is_same_v<i_2::type, int>);
   static_assert(i_2::value == 2);
-  static_assert(std::is_same<i_2::type, int>::value);
+  using i_3 = add<i_1, i_2>;
+  static_assert(std::is_same_v<i_3::type, int>);
+  static_assert(i_3::value == 3);
+  using i_4 = increment<i_3>;
+  static_assert(std::is_same_v<i_4::type, int>);
+  static_assert(i_4::value == 4);
+  using i_6 = constant<int, 6>;
+  static_assert(std::is_same_v<i_6::type, int>);
+  static_assert(i_6::value == 6);
+  using i_5 = decrement<i_6>;
+  static_assert(std::is_same_v<i_5::type, int>);
+  static_assert(i_5::value == 5);
+  using i_8 = multiply<i_2, i_4>;
+  static_assert(std::is_same_v<i_8::type, int>);
+  static_assert(i_8::value == 8);
+  using i_7 = divide<constant<int, 15>, i_2>;
+  static_assert(std::is_same_v<i_7::type, int>);
+  static_assert(i_7::value == 7);
 
   using u8_255 = constant<std::uint8_t, 255>;
-  using u8_0 = u8_255::inc;
+  using u8_0 = increment<u8_255>;
   static_assert(u8_0::value == 0u);
-  static_assert(std::is_same<u8_0::type, std::uint8_t>::value);
+  static_assert(std::is_same_v<u8_0::type, std::uint8_t>);
 }
 
 int main() {
