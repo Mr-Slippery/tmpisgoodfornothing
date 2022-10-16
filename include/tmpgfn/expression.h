dbebc9fcab946af18de4b305815d3e84c895d24e
Cornel Izbasa <badc0ded@gmail.com>
2022-10-16 16:28:30 +0300

    Refactor expression validation

--- include/tmpgfn/expression.h
+++ include/tmpgfn/expression.h
@@ -9,25 +9,29 @@ namespace expressions {
 template <typename V, V v> struct expression {
   using type = V;
   static constexpr V value = v;
-  static constexpr bool is_expression = true;
 };
 
 template <typename V, V v,
           typename std::enable_if_t<std::is_integral<V>::value, bool> = true>
-struct constant : public expression<V, v> {
-  using type = V;
-  static constexpr V value = v;
-};
+struct constant : public expression<V, v> {};
+
 template <typename V> using one = constant<V, static_cast<V>(1)>;
 template <typename V> using zero = constant<V, static_cast<V>(0)>;
 
-template <typename E1, typename E2,
-          typename std::enable_if_t<E1::is_expression, bool> = true,
-          typename std::enable_if_t<E2::is_expression, bool> = true,
+template <typename E,
           typename std::enable_if_t<
-              std::is_same_v<typename E1::type, typename E2::type>, bool> =
-              true>
-struct validate_expressions {};
+              std::is_base_of_v<expression<typename E::type, E::value>, E>,
+              bool> = true>
+struct validate_expression {};
+
+template <
+    typename E1, typename E2,
+    typename std::enable_if_t<
+        std::is_base_of_v<expression<typename E2::type, E2::value>, E2>, bool> =
+        true,
+    typename std::enable_if_t<
+        std::is_same_v<typename E1::type, typename E2::type>, bool> = true>
+struct validate_expressions : validate_expression<E1> {};
 
 template <typename E1, typename E2>
 struct add
@@ -36,9 +40,8 @@ struct add
                         static_cast<typename E1::type>(E1::value + E2::value)> {
 };
 
-template <typename E1,
-          typename std::enable_if_t<E1::is_expression, bool> = true>
-struct increment : add<E1, one<typename E1::type>> {};
+template <typename E>
+struct increment : public add<E, one<typename E::type>> {};
 
 template <typename E1, typename E2>
 struct subtract
@@ -47,9 +50,8 @@ struct subtract
                         static_cast<typename E1::type>(E1::value - E2::value)> {
 };
 
-template <typename E1,
-          typename std::enable_if_t<E1::is_expression, bool> = true>
-struct decrement : subtract<E1, one<typename E1::type>> {};
+template <typename E>
+struct decrement : public subtract<E, one<typename E::type>> {};
 
 template <typename E1, typename E2>
 struct multiply
